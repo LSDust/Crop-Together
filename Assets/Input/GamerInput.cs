68ce3f9144203f9f1840db9ee14353830cf39ab9
辰暮 <2250919876@qq.com>
2025-06-18 23:07:10 +0800

    添加Input System
    1.添加Input System并配置完善
    2.修改.gitignore忽略.idea文件夹

---
+++ Assets/Input/GamerInput.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace CropTogether.Input
+{
+    public class GamerInput : MonoBehaviour
+    {
+        private static GamerInput _instance;
+        private InputControls _inputControls;
+
+        public static InputControls Controls => Instance._inputControls;
+
+        private static GamerInput Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var gameObject = new GameObject(nameof(GamerInput));
+                    _instance = gameObject.AddComponent<GamerInput>();
+                    DontDestroyOnLoad(gameObject);
+                }
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            _inputControls = new InputControls();
+            _inputControls.Enable();
+        }
+
+        private void OnDestroy()
+        {
+            _inputControls?.Dispose();
+        }
+    }
+}
